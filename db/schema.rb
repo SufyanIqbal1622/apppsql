969a171fcc55887b31fabee85c322742bb7fbba5
SufyanIqbal1622 <bcsf16m16@gmai.com>
2021-01-16 12:18:09 +0500

    Associassion one to many added between user & post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_122123) do
+ActiveRecord::Schema.define(version: 2021_01_16_071456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_01_15_122123) do
     t.text "contect"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema.define(version: 2021_01_15_122123) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "products", "users"
 end
